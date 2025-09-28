f961752fb988a4a5b19ace5e26806730fedb5267
Mehroz farooq <mehrozfarooq127@gmail.com>
2025-09-28 11:48:52 +0500

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,10 @@
-import express from "express";
-import cors from "cors";
-import morgan from "morgan";
-import { connectMongo } from "./lib/mongo.js";
-import AuthRouter from "./routes/authRoutes.js";
-import QnaRouter from "./routes/qnaRoutes.js";
-import dotenv from "dotenv";
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import { connectMongo } from './lib/mongo.js';
+import AuthRouter from './routes/authRoutes.js';
+import QnaRouter from './routes/qnaRoutes.js';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
@@ -14,27 +14,9 @@ app.use((req, res, next) => {
   console.log("ENV :", process.env.CLIENT_ORIGIN);
   next();
 });
-const allowedOrigins = [
-  process.env.CLIENT_ORIGIN,
-  "https://qwizzy-zeta.vercel.app/", // local dev
-  "https://qwizzy-hcs9xw9p4-mehrozs-projects-d1b57550.vercel.app/", // prod,
-  "https://qwizzy-git-main-mehrozs-projects-d1b57550.vercel.app",
-  "https://qwizzy-2ewiupg17-mehrozs-projects-d1b57550.vercel.app",
-];
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
-app.use(express.json({ limit: "1mb" }));
-app.use(morgan("dev"));
+app.use(cors({ origin: true, credentials: true }));
+app.use(express.json({ limit: '1mb' }));
+app.use(morgan('dev'));
 
 app.use("/api/auth", AuthRouter);
 app.use("/api/qna", QnaRouter);
@@ -46,15 +28,13 @@ app.get("/health", (_req, res) => {
 app.get("/", (_req, res) => {
   res.send("server is running");
 });
-connectMongo();
+  connectMongo()
 
 // Local dev server (only runs outside Vercel)
 if (process.env.NODE_ENV !== "production") {
-  app.listen(process.env.PORT || 3000, () => {
-    console.log(
-      `✅ API listening on http://localhost:${process.env.PORT || 3000}`
-    );
-  });
+      app.listen(process.env.PORT || 3000, () => {
+        console.log(`✅ API listening on http://localhost:${process.env.PORT || 3000}`);
+      });
 }
 
 // Vercel picks up this Express app automatically
