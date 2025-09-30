be095c1abd1e833565e529a20ad213341a0f3e18
Mehroz101 <mehrozfarooq127@gmail.com>
2025-09-30 12:16:26 +0500

    pwa setub

---
+++ qna-hub-spark-main/manifest.ts
@@ -0,0 +1,41 @@
+export const manifestForPlugIn = {
+  registerType: 'prompt' as const,
+  includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+  manifest: {
+    name: "Qwizzy",
+    short_name: "Qwizzy",
+    description: "this is a question and answer app",
+    icons: [
+      {
+        src: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: '/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+        purpose: 'apple-touch-icon',
+      },
+      {
+        src: '/maskable_icon.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+    ],
+    theme_color: '#171717',
+    background_color: '#f0e7db',
+    display: "standalone",
+    scope: '/',
+    start_url: "/",
+    orientation: 'portrait-primary',
+  },
+};
--- qna-hub-spark-main/vite.config.ts
+++ qna-hub-spark-main/vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-
+import { manifestForPlugIn } from "./manifest";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger(), VitePWA(manifestForPlugIn)].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
